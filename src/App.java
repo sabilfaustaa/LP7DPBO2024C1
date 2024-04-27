9f0593032d0b021de4e20af7ebf73b5f3260a332
sabilfaustaa <61264687+sabilfaustaa@users.noreply.github.com>
2024-04-28 00:17:54 +0700

    clear all

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
+}
---
+++ src/FlappyBird.java
@@ -0,0 +1,124 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
+    int frameWidth = 360;
+    int frameHeight = 640;
+
+    int playerStartPosX = frameWidth / 8;
+    int playerStartPosY = frameHeight / 2;
+    int playerWidth = 34;
+    int playerHeight = 24;
+    Player player;
+
+    Image backgroundImage;
+    Image birdImage;
+    Image lowerPipeImage;
+    Image upperPipeImage;
+
+    int pipeStartPosX = frameWidth;
+    int pipeStartPosY = 0;
+    int pipeWidth = 64;
+    int pipeHeight = 512;
+    ArrayList<Pipe> pipes;
+
+    Timer gameLoop;
+    Timer pipesCooldown;
+    int gravity = 1;
+    public FlappyBird() {
+        setPreferredSize(new Dimension(frameWidth, frameHeight));
+        setBackground(Color.BLUE);
+        addKeyListener(this);
+
+        loadImageAssets();
+
+        player = new Player(playerStartPosX, playerStartPosY, playerWidth, playerHeight, birdImage);
+
+        gameLoop = new Timer(1000 / 60, this);
+        gameLoop.start();
+
+        player = new Player(playerStartPosX, playerStartPosY, playerWidth, playerHeight, birdImage);
+        pipes = new ArrayList<>();
+
+        pipesCooldown = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+        pipesCooldown.start();
+    }
+
+    private void loadImageAssets() {
+        backgroundImage = new ImageIcon(getClass().getResource("assets/background.png")).getImage();
+        birdImage = new ImageIcon(getClass().getResource("assets/bird.png")).getImage();
+        lowerPipeImage = new ImageIcon(getClass().getResource("assets/lowerPipe.png")).getImage();
+        upperPipeImage = new ImageIcon(getClass().getResource("assets/upperPipe.png")).getImage();
+    }
+
+    public void move () {
+        player.setVelocityY(player.getVelocityY() + gravity);
+        player.setPosY(player.getPosY() + player.getVelocityY());
+        player.setPosY(Math.max(player.getPosY(), 0));
+
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            pipe.setPosX(pipe.getPosX() + pipe.getVelocityX());
+        }
+    }
+
+    public void placePipes() {
+        int randomPipePosY = (int)(pipeStartPosY - pipeHeight / 4 - Math.random() * (pipeHeight / 2));
+        int openingSpace = frameHeight / 4;
+
+        Pipe upperPipe = new Pipe(pipeStartPosX, randomPipePosY, pipeWidth, pipeHeight, upperPipeImage);
+        pipes.add(upperPipe);
+
+        Pipe lowerPipe = new Pipe(pipeStartPosX, randomPipePosY + pipeHeight + openingSpace, pipeWidth, pipeHeight, lowerPipeImage);
+        pipes.add(lowerPipe);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            player.setVelocityY(-10);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+
+    private void draw(Graphics g) {
+        g.drawImage(backgroundImage, 0, 0, frameWidth, frameHeight, null);
+        g.drawImage(player.getImage(), player.getPosX(), player.getPosY(), player.getWidth(), player.getHeight(), null);
+
+        for (int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.getImage(), pipe.getPosX(), pipe.getPosY(), pipe.getWidth(), pipe.getHeight(), null);
+        }
+    }
+}
